1946bb74d810435e89fcc653c1c2eb4bcfe226e2
ibixler <108148378+ibixler@users.noreply.github.com>
2024-04-18 13:31:17 -0400

    last time

--- vite-project/src/App.svelte
+++ vite-project/src/App.svelte
@@ -1,49 +1,52 @@
-<html lang="en">
-  <head>
-    <style>
-      body { 
-        margin:0; 
-        background:black 
-      } 
-    </style>
-    <script>
-      let ws, msgs = [], ctx = null
-      
-      window.onload = function() {
-        let celebration;
-        ws = new WebSocket( 'ws://127.0.0.1:3000' )
+<style>
+  body {
+    margin: 0;
+    background: black;
+  }
+</style>
+<script>
+  import { onMount } from "svelte";
+  let celebration;
+  let ws,
+    msgs = [],
+    ctx = null;
 
-        ws.onopen = () => {
-          ws.onmessage = async msg => {
-            const pos = await msg.data.text()
-            const [x,y] = pos.split( ':' ).map( v => parseInt(v) )
+  window.onload = function () {
+    ws = new WebSocket("ws://127.0.0.1:3000");
 
-            ctx.fillStyle = 'red'
-            ctx.fillRect( x,y,50,50 )
-          }
-        }
+    ws.onopen = () => {
+      celebration.play();
+      ws.onmessage = async (msg) => {
+        const pos = await msg.data.text();
+        const [x, y] = pos.split(":").map((v) => parseInt(v));
 
-        const canvas = document.querySelector('canvas')
-        canvas.width = window.innerWidth
-        canvas.height = window.innerHeight
-        ctx = canvas.getContext( '2d' )
+        ctx.fillStyle = "red";
+        ctx.fillRect(x, y, 50, 50);
+      };
+    };
 
-        window.onclick = e => {
-          console.log(e.pageX, e.pageY);
-          ws.send( `${e.pageX-50}:${e.pageY-50}` )
-          ctx.fillStyle = 'yellow'
-          ctx.fillRect( e.pageX-50,e.pageY-50,50,50 )
-        }
-      }
-    </script>
-  </head>
-  <!-- <audio
-    src='https://sveltejs.github.io/assets/music/strauss.mp3'
-    preload="auto"
-    bind:this={celebration}
-    controls
-  > -->
-  <body>
-    <canvas></canvas>
-  </body>
-</html>
+    const canvas = document.querySelector("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    ctx = canvas.getContext("2d");
+
+    window.onclick = (e) => {
+      console.log(e.pageX, e.pageY);
+      ws.send(`${e.pageX - 50}:${e.pageY - 50}`);
+      ctx.fillStyle = "yellow";
+      ctx.fillRect(e.pageX - 50, e.pageY - 50, 50, 50);
+    };
+  };
+</script>
+
+<audio
+  src="https://sveltejs.github.io/assets/music/strauss.mp3"
+  preload="auto"
+  bind:this={celebration}
+  hidden
+></audio>
+
+<body>
+  <canvas></canvas>
+</body>
+
